a86272149f29ecc299d97f0e1d94a4c819d0b332
AndrezaCarrilho <108102040+AndrezaCarrilho@users.noreply.github.com>
2025-06-15 21:35:33 +0000

    update telas e adição de pastas

--- condominio/src/app/page.tsx
+++ condominio/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/login')  // Redireciona para a tela de login
+  redirect('/Login')  // Redireciona para a tela de login
 }
